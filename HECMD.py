64cee336d9b5f2db7845e4edc246cc41e17c5db7
zhuhong <zhuhongaaa@gmail.com>
2013-11-08 20:45:30 -0800

    sort it

--- HECMD.py
+++ HECMD.py
@@ -2,7 +2,7 @@
 
 '''
 author: zhuh
-date: 2012.08.16
+date: 2013.10.30
 version: 0.0.1
 '''
 
@@ -15,9 +15,6 @@ import struct
 import numpy as np
 import MDAnalysis
 import MDAnalysis.core.qcprot as qcp
-#from entropical import libentropical
-# from MDPackage import Index
-
 import time as Time
 
 K= 1.3806504e-23
@@ -90,7 +87,135 @@ def Print_Index(index_list):
         print "Group %4d (%15s) has %6d elements" %(i , index_list[i].group_name , len(index_list[i].group_list))
 
 
-def QH_entro(top_file,traj_file,index_file,temperature=300,skip=1,begin=0,end=-1,CYCLES=1):
+def QHE(traj_data2,group_mass,cycle_frame):
+    traj_ave  = np.zeros((3*natoms),dtype=np.float32)
+    covar_mat = np.zeros((3*natoms,3*natoms),dtype=np.float32)
+
+    for i in range(cycle_frame):
+        traj_ave += traj_data2[i]
+
+    traj_ave = traj_ave / cycle_frame
+
+    for i in range(cycle_frame):
+        _delta_vect=traj_data2[i]-traj_ave
+        _mass_delta_vect=_delta_vect*group_mass
+        covar_mat += np.matrix(_mass_delta_vect).T * np.matrix(_mass_delta_vect)
+        sys.stderr.write("\t Reading frame %8d\r" %(i+1))
+        sys.stderr.flush()
+        
+    # covar_mat = covar_mat / ( nframes -1 )   # be careful!
+    covar_mat = covar_mat / cycle_frame
+     
+    # TIME1=Time.time()
+    # print "\t Time used %6.2f s" %(TIME1 - BEGIN_TIME)
+
+
+    print "step 4: Diagnoalizing the covariance matrix, cycling %d" %cycle
+    eigenvalues,eigenvectors = np.linalg.eig(covar_mat)
+    #    print eigenvalues
+    # np.savetxt("eigenvalues.dat",eigenvalues)
+    # sys.exit()
+    # TIME2=Time.time()    
+    # print "\t Time used %6.2f s" %(TIME2 - TIME1)
+
+
+    eigen_thr = 1e-5
+    truncate_num = 0
+    for i in range(3*natoms):
+        if eigenvalues[i] < eigen_thr:
+            truncate_num = i
+            break
+        elif eigenvalues[-1] > eigen_thr:
+            truncate_num = 3*natoms
+    print "\t Using %d eigenvalues to calculate entropy." %truncate_num
+        
+    
+    print "step 5: Calculating the quasiharmonic entropy, cycling %d" %cycle
+    global HBA
+    global K
+    global MP
+    global RC
+    alpha_const  =HBA*(1e10)/math.sqrt(K*temperature*MP)
+    # eigval_class = eigenvalues[:truncate_num]
+    # eigvec_class = eigenvectors[:truncate_num]
+    alpha     =[alpha_const/math.sqrt(eigenvalues[i]) for i in range(truncate_num)]
+    s_quantum =[alpha[i]/(math.exp(alpha[i])-1) - math.log(1-math.exp(-alpha[i])) for i in range(truncate_num)]
+    # np.savetxt("quantum.dat",s_quantum)
+    
+    total_entropy=sum(s_quantum)*RC
+    print "\t Entopy: %8.2f J/mol/K, %8.2f Kcal/mol." %(total_entropy,total_entropy/4186.8*temperature)
+    fp.write("\t Entopy: %8.2f J/mol/K, %8.2f Kcal/mol.\n" %(total_entropy,total_entropy/4186.8*temperature))
+    fp.flush()
+
+
+######################
+    print "step 6: Calculating the anhormonic entropy"
+   
+    _bp_temp = [eigenvectors[:,i]*group_mass for i in range(truncate_num)]   
+    # print np.shape(_bp_temp)
+    _delta_temp= np.zeros((3*natoms,nframes),dtype=np.float32)
+    for i in range(nframes):
+        for j in range(3*natoms):
+            _delta_temp[j,i]=traj_data2[i][j]-traj_ave[j]
+    bp=np.matrix(_bp_temp)*np.matrix(_delta_temp)
+    for i in range(truncate_num):
+        np.savetxt("dist_%d.dat" %(i+1),bp[i])
+    # print np.shape(bp)
+#    print bp[0]
+#   print bp[10]
+    anh=np.zeros([truncate_num],dtype=np.float32)
+    _beta_const = 0.5*(1-math.log(H*HBA/K/temperature))
+    BIN_NUM = 100
+    rxy=np.zeros([truncate_num],dtype=np.float32)
+    # print _beta_const
+    for m in range(truncate_num):
+        _hist,_bins=np.histogram(bp[m],bins=np.linspace(np.amin(bp[m]),np.amax(bp[m]),BIN_NUM+1))   
+        _prob = [(_item+0.01)/sum([_hist[i]*(_bins[i+1]-_bins[i]) for i in range(len(_hist))]) for _item in _hist]
+        _int_prob =sum([math.log(_prob[i]*1e10/math.sqrt(MP))*_prob[i]*(_bins[i+1]-_bins[i]) for i in range(len(_hist))]) #care the unit
+        # print _prob
+
+        # for f(a)
+        
+        _fa = [ math.sqrt(abs(-1*eigenvalues[m]*math.log(_prob_item*math.sqrt(2*math.pi*eigenvalues[m])))) for _prob_item in _prob ]
+        _x = np.zeros([len(_prob)],dtype=np.float32)
+        _y = np.zeros([len(_prob)],dtype=np.float32)
+        for kk in range(len(_prob)):
+            _x[kk] = 0.5*(_bins[kk]+_bins[kk+1])
+            if _x[kk] < 0 :
+                _y[kk] = -1* _fa[kk]
+            else:
+                _y[kk] = _fa[kk]
+        _x_ave = sum(_x) / BIN_NUM
+        _y_ave = sum(_y) / BIN_NUM
+        rxy[m] = sum([(_x[kk]-_x_ave)*(_y[kk]-_y_ave) for kk in range(BIN_NUM)])\
+        /math.sqrt(sum([(_x[kk]-_x_ave)**2 for kk in range(BIN_NUM)])\
+            *(sum([(_y[kk]-_y_ave)**2 for kk in range(BIN_NUM)])))
+        
+        # end of f(a)
+        # sys.exit()
+
+
+        anh[m] = _beta_const - _int_prob
+    # print anh
+    np.savetxt("R_value.dat",rxy)
+    print "Writed R vaule to R_value.dat"
+    # np.savetxt('anh.dat',anh)     
+    _delta = 0.
+    for m in range(10):
+        _delta += s_quantum[m] - anh[m]
+    Delta_S = _delta * RC
+    print "\t Delta_anh: %8.2f J/mol/K, %8.2f Kcal/mol." %(Delta_S, Delta_S/4186.8*temperature)
+    print "After correction: %8.2f J/mol/K, %8.2f Kcal/mol." %(total_entropy- Delta_S, (total_entropy-Delta_S)/4186.8*temperature)
+    # bp = numpy.zeros((truncate_num,nframes),dtype=numpy.float32)
+    # for i in range(truncate_num):
+    #         bp[i,k]
+
+    # print bq[0]
+
+
+
+
+def Main(top_file,traj_file,index_file,temperature=300,skip=1,begin=0,end=-1,CYCLES=1):
     '''
     Add some words here.
     '''
@@ -231,133 +356,14 @@ def QH_entro(top_file,traj_file,index_file,temperature=300,skip=1,begin=0,end=-1
     fp = open("S-t.dat",'w')
 
     for cycle in range(CYCLES):
-        cycle_frame = nframes/CYCLES*(cycle+1)
-        for i in range(cycle_frame):
-            traj_ave += traj_data2[i]
+        QHE(traj_data2,group_mass,nframes/CYCLES*(cycle+1))
 
-        traj_ave = traj_ave / cycle_frame
-
-        for i in range(cycle_frame):
-            _delta_vect=traj_data2[i]-traj_ave
-            _mass_delta_vect=_delta_vect*group_mass
-            covar_mat += np.matrix(_mass_delta_vect).T * np.matrix(_mass_delta_vect)
-            sys.stderr.write("\t Reading frame %8d\r" %(i+1))
-            sys.stderr.flush()
-        
-        # covar_mat = covar_mat / ( nframes -1 )   # be careful!
-        covar_mat = covar_mat / cycle_frame
-     
-        # TIME1=Time.time()
-        # print "\t Time used %6.2f s" %(TIME1 - BEGIN_TIME)
-
-
-        print "step 4: Diagnoalizing the covariance matrix, cycling %d" %cycle
-        eigenvalues,eigenvectors = np.linalg.eig(covar_mat)
-    #    print eigenvalues
-        np.savetxt("eigenvalues.dat",eigenvalues)
-        # sys.exit()
-    
-        # TIME2=Time.time()
-    
-    
-        # print "\t Time used %6.2f s" %(TIME2 - TIME1)
-
-
-        eigen_thr = 1e-5
-        truncate_num = 0
-        for i in range(3*natoms):
-            if eigenvalues[i] < eigen_thr:
-                truncate_num = i
-                break
-            elif eigenvalues[-1] > eigen_thr:
-                truncate_num = 3*natoms
-        print "\t Using %d eigenvalues to calculate entropy." %truncate_num
-        
-    
-        print "step 5: Calculating the quasiharmonic entropy, cycling %d" %cycle
-        global HBA
-        global K
-        global MP
-        global RC
-        alpha_const  =HBA*(1e10)/math.sqrt(K*temperature*MP)
-        # eigval_class = eigenvalues[:truncate_num]
-        # eigvec_class = eigenvectors[:truncate_num]
-
-        alpha     =[alpha_const/math.sqrt(eigenvalues[i]) for i in range(truncate_num)]
-        s_quantum =[alpha[i]/(math.exp(alpha[i])-1) - math.log(1-math.exp(-alpha[i])) for i in range(truncate_num)]
-        # np.savetxt("quantum.dat",s_quantum)
-    
-        total_entropy=sum(s_quantum)*RC
-        print "\t Entopy: %8.2f J/mol/K, %8.2f Kcal/mol." %(total_entropy,total_entropy/4186.8*temperature)
-        fp.write("\t Entopy: %8.2f J/mol/K, %8.2f Kcal/mol.\n" %(total_entropy,total_entropy/4186.8*temperature))
-        fp.flush()
     fp.close()
         # sys.exit()
 
 ##########################################
     
-    print "step 6: Calculating the anhormonic entropy"
-   
-    _bp_temp = [eigenvectors[:,i]*group_mass for i in range(truncate_num)]   
-    # print np.shape(_bp_temp)
-    _delta_temp= np.zeros((3*natoms,nframes),dtype=np.float32)
-    for i in range(nframes):
-        for j in range(3*natoms):
-            _delta_temp[j,i]=traj_data2[i][j]-traj_ave[j]
-    bp=np.matrix(_bp_temp)*np.matrix(_delta_temp)
-    for i in range(truncate_num):
-        np.savetxt("dist_%d.dat" %(i+1),bp[i])
-    # print np.shape(bp)
-#    print bp[0]
-#   print bp[10]
-    anh=np.zeros([truncate_num],dtype=np.float32)
-    _beta_const = 0.5*(1-math.log(H*HBA/K/temperature))
-    BIN_NUM = 100
-    rxy=np.zeros([truncate_num],dtype=np.float32)
-    # print _beta_const
-    for m in range(truncate_num):
-        _hist,_bins=np.histogram(bp[m],bins=np.linspace(np.amin(bp[m]),np.amax(bp[m]),BIN_NUM+1))   
-        _prob = [(_item+0.01)/sum([_hist[i]*(_bins[i+1]-_bins[i]) for i in range(len(_hist))]) for _item in _hist]
-        _int_prob =sum([math.log(_prob[i]*1e10/math.sqrt(MP))*_prob[i]*(_bins[i+1]-_bins[i]) for i in range(len(_hist))]) #care the unit
-        # print _prob
 
-        # for f(a)
-        
-        _fa = [ math.sqrt(abs(-1*eigenvalues[m]*math.log(_prob_item*math.sqrt(2*math.pi*eigenvalues[m])))) for _prob_item in _prob ]
-        _x = np.zeros([len(_prob)],dtype=np.float32)
-        _y = np.zeros([len(_prob)],dtype=np.float32)
-        for kk in range(len(_prob)):
-            _x[kk] = 0.5*(_bins[kk]+_bins[kk+1])
-            if _x[kk] < 0 :
-                _y[kk] = -1* _fa[kk]
-            else:
-                _y[kk] = _fa[kk]
-        _x_ave = sum(_x) / BIN_NUM
-        _y_ave = sum(_y) / BIN_NUM
-        rxy[m] = sum([(_x[kk]-_x_ave)*(_y[kk]-_y_ave) for kk in range(BIN_NUM)])\
-        /math.sqrt(sum([(_x[kk]-_x_ave)**2 for kk in range(BIN_NUM)])\
-            *(sum([(_y[kk]-_y_ave)**2 for kk in range(BIN_NUM)])))
-        
-        # end of f(a)
-        # sys.exit()
-
-
-        anh[m] = _beta_const - _int_prob
-    # print anh
-    np.savetxt("R_value.dat",rxy)
-    print "Writed R vaule to R_value.dat"
-    # np.savetxt('anh.dat',anh)     
-    _delta = 0.
-    for m in range(10):
-        _delta += s_quantum[m] - anh[m]
-    Delta_S = _delta * RC
-    print "\t Delta_anh: %8.2f J/mol/K, %8.2f Kcal/mol." %(Delta_S, Delta_S/4186.8*temperature)
-    print "After correction: %8.2f J/mol/K, %8.2f Kcal/mol." %(total_entropy- Delta_S, (total_entropy-Delta_S)/4186.8*temperature)
-    # bp = numpy.zeros((truncate_num,nframes),dtype=numpy.float32)
-    # for i in range(truncate_num):
-    #         bp[i,k]
-
-    # print bq[0]
 
 
 def Usage():
@@ -394,7 +400,7 @@ def CheckArgs():
 
 
         if os.path.isfile(traj_file) and os.path.isfile(top_file) and os.path.isfile(index_file):
-            QH_entro(top_file,traj_file,index_file,temperature,skip,begin_time,end_time,cycles)
+            Main(top_file,traj_file,index_file,temperature,skip,begin_time,end_time,cycles)
     else:
         Usage()
         sys.exit()
